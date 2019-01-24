772963136cdd206818641ed7ac73a855c36c5852
Paulo <p.henriquerm@hotmail.com>
2019-01-24 17:56:59 -0300

    implemetando função jokenpo

--- App.js
+++ App.js
@@ -5,17 +5,23 @@ export default class app3 extends Component {
 
   constructor(props){
     super(props);
+
+    this.state = { escolhaUsuario : '' };
+  }
+
+  jokenpo(escolhaUsuario){
+    this.setState({escolhaUsuario});
   }
 
   render () {
     return(
       <View>
-        <Text>Escolha do computador</Text>
-        <Text>Escolha do usuário</Text>
+        <Text>Escolha do computador: </Text>
+        <Text>Escolha do usuário: {this.state.escolhaUsuario}</Text>
         <Text>Resultado</Text>
-        <Button title="Pedra" />
-        <Button title="Papel" />
-        <Button title="Tesoura" />
+        <Button title="Pedra" onPress={ () => { this.jokenpo('pedra') } } />
+        <Button title="Papel" onPress={ () => { this.jokenpo('papel') } } />
+        <Button title="Tesoura" onPress={ () => { this.jokenpo('tesoura') } } />
       </View>
     );
   }
