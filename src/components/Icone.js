2d751e9e171529d80cefc0c95f6901c5e865868a
Paulo <p.henriquerm@hotmail.com>
2019-01-25 16:18:14 -0300

    Formatando codigo

--- src/components/Icone.js
+++ src/components/Icone.js
@@ -1,53 +1,48 @@
-import React, { Component } from 'react';
-import {
-    View,
-    Text,
-    Image,
-    StyleSheet,
-} from 'react-native';
+import React, { Component } from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
 
 export default class Icone extends Component {
-    render() {
-        //this.props.escolha
-        //this.props.jogador
+	render() {
+		//this.props.escolha
+		//this.props.jogador
 
-        if (this.props.escolha == "pedra") {
-            return (
-                <View style={styles.icone}>
-                    <Text style={styles.txtJogador}>{this.props.jogador}</Text>
-                    <Image source={require('../../imgs/pedra.png')} />
-                </View>
-            );
-        }
-        else if (this.props.escolha == "papel") {
-            return (
-                <View style={styles.icone}>
-                    <Text style={styles.txtJogador}>{this.props.jogador}</Text>
-                    <Image source={require('../../imgs/papel.png')} />
-                </View>
-            );
-        }
-        else if (this.props.escolha == "tesoura") {
-            return (
-                <View style={styles.icone}>
-                    <Text style={styles.txtJogador}>{this.props.jogador}</Text>
-                    <Image source={require('../../imgs/tesoura.png')} />
-                </View>
-            );
-        }
-        else {
-            return false;
-        }
+		if (this.props.escolha == "pedra") {
+			return (
+				<View style={styles.icone}>
+					<Text style={styles.txtJogador}>{this.props.jogador}</Text>
+					<Image source={require("../../imgs/pedra.png")} />
+				</View>
+			);
+		}
+		else if (this.props.escolha == "papel") {
+			return (
+				<View style={styles.icone}>
+					<Text style={styles.txtJogador}>{this.props.jogador}</Text>
+					<Image source={require("../../imgs/papel.png")} />
+				</View>
+			);
+		}
+		else if (this.props.escolha == "tesoura") {
+			return (
+				<View style={styles.icone}>
+					<Text style={styles.txtJogador}>{this.props.jogador}</Text>
+					<Image source={require("../../imgs/tesoura.png")} />
+				</View>
+			);
+		}
+		else {
+			return false;
+		}
 
-    }
+	}
 }
 
 const styles = StyleSheet.create({
-    icone: {
-        alignItems: 'center',
-        marginBottom: 20
-    },
-    txtJogador: {
-        fontSize: 18
-    }
+	icone: {
+		alignItems: "center",
+		marginBottom: 20
+	},
+	txtJogador: {
+		fontSize: 18
+	}
 });
--- src/components/Topo.js
+++ src/components/Topo.js
@@ -1,15 +1,11 @@
-import React, {Component} from 'react';
-import { 
-    View, 
-    Image, 
-    StyleSheet,
-} from 'react-native';  
+import React, {Component} from "react";
+import { View, Image } from "react-native";  
 export default class Topo  extends Component {
-    render(){
-        return (
-            <View >
-                <Image source={ require('../../imgs/jokenpo.png') } />
-            </View>
-        );
-    }
+	render(){
+		return (
+			<View >
+				<Image source={ require("../../imgs/jokenpo.png") } />
+			</View>
+		);
+	}
 }
